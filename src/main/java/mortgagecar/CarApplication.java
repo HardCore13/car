48ff271a2cab1a7def8d7959597a6feb80ede803
chenjinlong <chenjinlong@example.com>
2017-04-13 15:58:37 +0800

    demo

---
+++ src/main/java/mortgagecar/CarApplication.java
@@ -0,0 +1,13 @@
+package mortgagecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(CarApplication.class, args);
+	}
+
+}
