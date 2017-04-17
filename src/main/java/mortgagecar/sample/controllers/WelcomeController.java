eeeec22d634eba7b09f2edd03e901753eaa92bf9
chenjinlong <chenjinlong@example.com>
2017-04-17 18:44:36 +0800

    添加前端框架

--- src/main/java/mortgagecar/sample/controllers/WelcomeController.java
+++ src/main/java/mortgagecar/sample/controllers/WelcomeController.java
@@ -1,24 +1,34 @@
 package mortgagecar.sample.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 import java.util.Map;
 
 
-@Controller
+@RestController
 public class WelcomeController {
 
 	@Value("${application.message:Hello World}")
 	private String message = "Hello World";
 
-	@GetMapping("/")
-	public String welcome(Map<String, Object> model) {
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public ModelAndView welcome(Map<String, Object> model) {
 		model.put("time", new Date());
 		model.put("message", this.message);
-		return "sample/welcome";
+		return new ModelAndView("sample/welcome", model);
 	}
+
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index() {
+
+		return new ModelAndView("index");
+	}
+
 
 }
